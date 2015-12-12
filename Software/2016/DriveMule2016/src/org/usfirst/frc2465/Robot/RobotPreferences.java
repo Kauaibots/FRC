e031514ad7d8a51f18db8307db6c3e38350a59bb
Tythax <ponokaicaberto@gmail.com>
2015-12-11 17:48:39 -1000

    This project is the prototype drive system for 2016. Meccanum drive, NavX, and speed control with TalonSRXs.
    Previously:
    N/A
    
    Currently:
    Brakemode isn't working and due to that complication it is affecting the
    PID tuning on the robot.

---
+++ Software/2016/DriveMule2016/src/org/usfirst/frc2465/Robot/RobotPreferences.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.usfirst.frc2465.Robot;
+
+import edu.wpi.first.wpilibj.Preferences;
+
+/**
+ *
+ * @author Scott
+ */
+public class RobotPreferences {
+    
+    // PID Controller Settings
+    
+    static public double getAutoRotateP() {
+        return Preferences.getInstance().getDouble("AutoRotateP", 0.00130);
+    }
+    static public double getAutoRotateI() {
+        return Preferences.getInstance().getDouble("AutoRotateI", 0.00002);
+    }
+    static public double getAutoRotateD() {
+        return Preferences.getInstance().getDouble("AutoRotateD", 0.00);
+    }
+    static public double getAutoRotateOnTargetToleranceDegrees() {
+        return Preferences.getInstance().getDouble("AutoRotateOnTargetToleranceDegrees", 2.0);
+    }
+    static public double getAutoRotateDefaultTaretDegrees() {
+        return Preferences.getInstance().getDouble("AutoRotateDefaultTargetDegrees",0.0);
+    }
+}
