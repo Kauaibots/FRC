9b6e177b6ad76d61b023025fa2dd3c9e83889498
Tythax <ponokaicaberto@gmail.com>
2015-12-11 18:07:10 -1000

    Hi

--- Software/2016/DriveMule2016/src/org/usfirst/frc2465/Robot/Robot.java
+++ Software/2016/DriveMule2016/src/org/usfirst/frc2465/Robot/Robot.java
@@ -24,7 +24,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * functions corresponding to each mode, as described in the IterativeRobot
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the manifest file in the resource
- * directory.
+ * directory. :)
  */
 public class Robot extends IterativeRobot {
 
